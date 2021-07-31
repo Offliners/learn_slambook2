63239c161c0b6bc54f24beabe4430d4001867be2
Offliner <42777325+Offliners@users.noreply.github.com>
2021-07-31 22:03:09 +0800

    Create stereoVision.cpp

---
+++ ch5/stereo/stereoVision.cpp
@@ -0,0 +1,105 @@
+#include<opencv2/opencv.hpp>
+#include<vector>
+#include<string>
+#include<Eigen/Core>
+#include<pangolin/pangolin.h>
+#include<unistd.h>
+
+using namespace std;
+using namespace Eigen;
+
+// 文件路徑
+string left_file = "./left.png";
+string right_file = "./right.png";
+
+// 在pangolin中畫圖，已寫好，無須調整
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
+
+int main(int argc, char **argv)
+{
+    // 內參
+    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
+    // 基線
+    double b = 0.573;
+
+    // 讀取圖像
+    cv::Mat left = cv::imread(left_file, 0);
+    cv::Mat right = cv::imread(right_file, 0);
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的參數
+    cv::Mat disparity_sgbm, disparity;
+    sgbm->compute(left, right, disparity_sgbm);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
+
+    // 生成點雲
+    vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
+
+    // 如果你的機器慢，請把後面的++v和++u改成v+=2, u+=2
+    for (int v = 0; v < left.rows; ++v)
+    {
+        for (int u = 0; u < left.cols; ++u)
+        {
+            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+
+            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维為xyz,第四维為顏色
+
+            // 根据雙目模型計算 point 的位置
+            double x = (u - cx) / fx;
+            double y = (v - cy) / fy;
+            double depth = fx * b / (disparity.at<float>(v, u));
+            point[0] = x * depth;
+            point[1] = y * depth;
+            point[2] = depth;
+
+            pointcloud.push_back(point);
+        }
+    }
+    cv::imshow("disparity", disparity / 96.0);
+    cv::waitKey(0);
+    // 畫出點雲
+    showPointCloud(pointcloud);
+    return 0;
+}
+
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud)
+{
+    if (pointcloud.empty())
+    {
+        cerr << "Point cloud is empty!" << endl;
+        return;
+    }
+
+    pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+        .SetHandler(new pangolin::Handler3D(s_cam));
+
+    while (pangolin::ShouldQuit() == false)
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        for (auto &p: pointcloud)
+        {
+            glColor3f(p[3], p[3], p[3]);
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
+    return;
+}
