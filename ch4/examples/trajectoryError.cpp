8fefdc5ffc2aeb841f615917a367ef793921b8af
Offliner <42777325+Offliners@users.noreply.github.com>
2021-08-06 08:55:25 +0800

    Update trajectoryError.cpp

--- ch4/examples/trajectoryError.cpp
+++ ch4/examples/trajectoryError.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<unistd.h>
 #include<pangolin/pangolin.h>
-#include<sophus/se3.h>
+#include<sophus/se3.hpp>
 
 using namespace Sophus;
 using namespace std;
@@ -10,7 +10,7 @@ using namespace std;
 string groundtruth_file = "./examples/groundtruth.txt";
 string estimated_file = "./examples/estimated.txt";
 
-typedef vector<SE3, Eigen::aligned_allocator<SE3> > TrajectoryType;
+typedef vector<SE3d, Eigen::aligned_allocator<SE3d> > TrajectoryType;
 
 void DrawTrajectory(const TrajectoryType &gt, const TrajectoryType &esti);
 
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
     double rmse = 0;
     for (size_t i = 0; i < estimated.size(); ++i) 
     {
-        Sophus::SE3 p1 = estimated[i], p2 = groundtruth[i];
+        Sophus::SE3d p1 = estimated[i], p2 = groundtruth[i];
         double error = (p2.inverse() * p1).log().norm();
         rmse += error * error;
     }
@@ -54,7 +54,7 @@ TrajectoryType ReadTrajectory(const string &path)
     {
         double time, tx, ty, tz, qx, qy, qz, qw;
         fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
-        Sophus::SE3 p1(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz));
+        Sophus::SE3d p1(Eigen::Quaterniond(qw, qx, qy, qz), Eigen::Vector3d(tx, ty, tz));
         trajectory.push_back(p1);
     }
     return trajectory;
